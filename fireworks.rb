1c9f94ba2ac139c1c4195ab1f89778dd22fae991
leachj <mail@elletwo.co.uk>
2013-10-22 19:05:49 +0000

    adding firing relay

--- fireworks.rb
+++ fireworks.rb
@@ -3,10 +3,11 @@ require 'pi_piper'
 require './dmx'
 include PiPiper
 
-armingSwitch = PiPiper::Pin.new(:pin => 1, :direction => :in, :pull => :up)
-fireButton = PiPiper::Pin.new(:pin => 0, :direction => :in, :pull => :up)
+armingSwitch = PiPiper::Pin.new(:pin => 3, :direction => :in, :pull => :up)
+fireButton = PiPiper::Pin.new(:pin => 2, :direction => :in, :pull => :up)
 
 fireButtonLight = PiPiper::Pin.new(:pin => 8, :direction => :out)
+fireRelay = PiPiper::Pin.new(:pin => 7, :direction => :out)
 
 dmx = Dmx.new({:numbers => 1, :litebar => 8})
 number = NumberDisplay.new(dmx,:numbers)
@@ -17,7 +18,7 @@ selected = nil
 
 litebar.green()
 
-PiPiper::after :pin => 1, :goes => :high do
+PiPiper::after :pin => 3, :goes => :high do
 Thread.new do
   puts "Disarmed"
   armed = false
@@ -26,7 +27,7 @@ Thread.new do
 end
 end
 
-PiPiper::after :pin => 1, :goes => :low do
+PiPiper::after :pin => 3, :goes => :low do
 Thread.new do
    puts "Armed"
   armed = true
@@ -38,7 +39,7 @@ Thread.new do
 end
 end
 
-PiPiper::after :pin => 0, :goes => :low do
+PiPiper::after :pin => 2, :goes => :low do
 Thread.new do
   puts "Fire button pressed"
   if armed and selected
@@ -57,6 +58,9 @@ Thread.new do
      number.display(0)
      sleep 1
      number.clear()
+     fireRelay.on
+     sleep 5
+     fireRelay.off
      selected = nil
   elsif armed
     puts "no firework selected"
@@ -69,7 +73,7 @@ end
 
 pins = [PiPiper::Pin.new(:pin => 4, :direction => :out),
 	PiPiper::Pin.new(:pin => 17, :direction => :out),
-	PiPiper::Pin.new(:pin => 21, :direction => :out),
+	PiPiper::Pin.new(:pin => 27, :direction => :out),
 	PiPiper::Pin.new(:pin => 22, :direction => :out),
 	PiPiper::Pin.new(:pin => 18, :direction => :out),
 	PiPiper::Pin.new(:pin => 23, :direction => :out),
@@ -77,14 +81,69 @@ pins = [PiPiper::Pin.new(:pin => 4, :direction => :out),
 	PiPiper::Pin.new(:pin => 25, :direction => :out)
 ]
 
+polarityChannel = pins[0]
+selectorChannel = pins[1]
+firstChannel = pins[2]
+secondChannel = pins[3]
 
-get '/select/:number' do
+polarityBox = pins[7]
+selectorBox = pins[6]
+firstBox = pins[5]
+secondBox = pins[4]
+
+
+get '/select/:box/:channel/:polarity' do
 
     if armed
         fireButtonLight.on
      end
-     selected = #{params[:number]}
-    "selected #{params[:number]}"
+     box = "#{params[:box]}".to_i
+     channel = "#{params[:channel]}".to_i
+     polarity = "#{params[:polarity]}".to_i
+     pins.each { |pin| pin.off }
+
+     if(box > 2)
+	selectorBox.off
+     	if((box % 2) == 0)
+		secondBox.off
+	else
+		secondBox.on
+	end
+     else
+	selectorBox.on
+     	if((box % 2) == 0)
+		firstBox.off
+	else
+		firstBox.on
+	end
+     end
+
+     if(channel > 2)
+        selectorChannel.on
+        if((channel % 2) == 0)
+                secondChannel.on
+        else
+                secondChannel.off
+        end
+     else
+        selectorChannel.off
+        if((channel % 2) == 0)
+                firstChannel.on
+        else
+                firstChannel.off
+        end
+     end
+
+     if(polarity > 1)
+	polarityBox.off
+	polarityChannel.on
+     else
+	polarityBox.on
+	polarityChannel.off
+     end
+
+    selected = true
+    "selected box: #{box} channel: #{channel}"
 
 end
 
